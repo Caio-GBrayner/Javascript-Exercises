13c9d1a076c123a90420bc6ee70e7bc6d8f099b4
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-01-27 19:30:32 -0300

    Assignment and Comparison Operators

---
+++ 01-07/js/script.js
@@ -0,0 +1,12 @@
+let a = 5;
+let b ="5";
+console.log(typeof a);
+
+console.log(typeof b);
+
+if(a == b){
+    console.log("Match: a and b are the same")
+}
+else{
+    console.log("No match: a and b are not the same");
+}
---
+++ 01-08/js/script.js
@@ -0,0 +1,26 @@
+let a = 5;
+let b = 4;
+console.log(typeof a);
+
+console.log(typeof b);
+
+if(a > b){
+    console.log("a is greater than b")
+}
+else if (a < b){
+    console.log("a is less than b");
+}
+
+if(a >= b){
+    console.log("a is greater than equals b")
+}
+else if (a <= b){
+    console.log("a is less than equals b");
+}
+
+if(a != b){
+    console.log("a is not equal b")
+}
+else if (a = b){
+    console.log("a is equals b");
+}
