a2d3dc262752b6e8849996f44d4780e5f4b53d8e
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-01-27 18:48:10 -0300

    The data Types

---
+++ 01-06/js/script.js
@@ -0,0 +1,46 @@
+//String
+let stringDemo = "This is a String";
+
+console.log('String: ', typeof stringDemo);
+
+//Int
+let numberDemo = 25;
+
+console.log('Number: ', typeof numberDemo);
+
+//Float
+let floatDemo = 25.5;
+
+console.log('float: ', typeof floatDemo);
+
+//Boolean
+let booleanDemo = true;
+
+console.log('boolean: ', typeof booleanDemo);
+
+//Null
+let nullDemo = null;
+
+console.log('null: ', typeof nullDemo);
+
+//Undefine
+let undefineDemo = undefined;
+
+console.log('Undefined: ', typeof undefineDemo);
+
+//Undefine2
+let undefineDemo2;
+
+console.log('Undefined: ', typeof undefineDemo2);
+
+//Objects
+const objects_Demo = {
+    width: 250,
+    heigth: 250,
+    color: "red"
+}
+
+console.log('Object', objects_Demo)
+
+const arrayDemo = ['Apple', 'Mango', 'Orange'];
+console.log('Array: ', typeof arrayDemo);
