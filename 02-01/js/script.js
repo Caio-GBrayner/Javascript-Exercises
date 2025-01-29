1ddf955bd7700f1660c377425347683dd233fd71
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-01-28 22:12:43 -0300

    Arrays

---
+++ 02-01/js/script.js
@@ -0,0 +1,12 @@
+let item = "Laptop";
+
+const myCollection = ["Desktop", item, 5, true];
+
+// myCollection [2]= "Tablet" ;
+
+myCollection [myCollection.length] = "Tablet";
+
+myCollection[9] = "Smart Phone";
+
+console.log(myCollection);
+console.log(myCollection[6]);
