f177bd0694810ff5bd290e9ad90a47f5dadf218d
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-02-04 10:09:17 -0300

    The loops in Modern JavaScript

---
+++ 03-11/js/loop.js
@@ -0,0 +1,51 @@
+//Type of Computers
+const computers = ["Desktop", "Laptop","Tablet","Smart Phone"];
+
+const objectList = {
+    item01: {
+        type: "Desktop",
+    },
+    item02: {
+        type: "Laptop",
+    },
+    item03: {
+        type: "Tablet",
+    },
+    item04: {
+        type: "Smart Phone",
+    },
+}
+
+//Create Elements
+const mainTag = document.querySelector("main");
+let unorderedList = document.createElement("ul");
+
+// for (let i = 0; i<computers.length; i ++) {
+//     let listItem = document.createElement("li");
+//     listItem.innerHTML = computers [i];
+//     unorderedList.append(listItem);
+// }
+
+//"For of" Loop
+
+// for (const item of computers) {
+//     let listItem = document.createElement("li");
+//     listItem.innerHTML = item;
+//     unorderedList.append(listItem);
+// }
+
+// computers.forEach((item) => {
+//     let listItem = document.createElement("li");
+//     listItem.innerHTML = item;
+//     unorderedList.append(listItem);
+// });
+
+//For in loop for objects
+for (const singleObject in objectList) {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = `Name: ${objectList[singleObject].type}`;
+    unorderedList.append(listItem);
+}
+
+//Append List
+mainTag.append(unorderedList);
